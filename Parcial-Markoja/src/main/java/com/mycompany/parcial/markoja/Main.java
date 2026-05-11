1acdc862de4792d323bb16f3f8aa052b03d953e9
Sebastián-Markoja <sebaacc11@gmail.com>
2026-05-11 00:11:53 -0300

    primer commit

---
+++ Parcial-Markoja/src/main/java/com/mycompany/parcial/markoja/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial.markoja;
+
+/**
+ *
+ * @author Seba
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
